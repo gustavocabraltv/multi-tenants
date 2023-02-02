4c92132d94cbeb6d47125873297cefd6ecba637b
dev <dev@MacBook-Pro-de-Heitor.local>
2023-02-02 15:52:42 -0300

    Dynamic Pages

---
+++ components/Seo.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+interface Props {
+    title: string
+    description: string
+}
+
+const Seo = ({ title, description }: Props) => {
+    return (
+        <Head>
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+            />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    )
+}
+
+export default Seo
